b191a065219ef60bb1c0923fa0364ff9855b1a73
Tanjil Ahamed <tanjil113355@gmail.com>
2023-10-03 15:14:39 +0600

    same new futured added in middle section

--- src/pages/Home/Right-container/RightCon.jsx
+++ src/pages/Home/Right-container/RightCon.jsx
@@ -1,14 +1,44 @@
-import { FaGoogle } from "react-icons/fa";
+import { FaGoogle, FaFacebookF, FaTwitter, FaInstagram } from "react-icons/fa";
+import qzone1 from '../../../assets/qZone1.png'
+import qzone2 from '../../../assets/qZone2.png'
+import qzone3 from '../../../assets/qZone3.png'
+import bg1 from '../../../assets/bg1.png'
+
 
 const RightCon = () => {
     return (
         <div>
             <div>
-                <h2>Login with</h2>
+                <h2 className="mb-3 font-bold text-xl">Login with</h2>
+                <button className="btn btn-outline w-full mb-2 text-teal-400">
+                    <FaGoogle></FaGoogle>
+                    Google
+                </button>
                 <button className="btn btn-outline w-full">
                     <FaGoogle></FaGoogle>
-                    Button
+                    Github
                 </button>
+            </div>
+            <div className="my-8">
+                <h2 className="mb-4 font-bold text-xl">Find Us On</h2>
+                <button className="btn bg-base-100 border-2 w-full border-gray-300 p-4">
+                    <FaFacebookF></FaFacebookF>
+                    Facebook
+                </button>
+                <button className="border-x-2 bg-base-100 btn w-full border-gray-300 p-4">
+                    <FaTwitter></FaTwitter>
+                    Twitter
+                </button>
+                <button className="border-2 bg-base-100 btn w-full border-gray-300 p-4">
+                    <FaInstagram></FaInstagram>
+                    instragram
+                </button>
+            </div>
+            <div className="bg-[#F3F3F3] p-3">
+                <h2 className="font-bold text-xl mb-3 ">Q-zone</h2>
+                <img src={qzone1} alt="" />
+                <img src={qzone2} alt="" />
+                <img src={qzone3} alt="" />
             </div>
         </div>
     );
